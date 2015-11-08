cbf86dff6618e8178e4ade28f4ae8053626c38b2
delaemon <takeshi.music.5@gmail.com>
2015-11-08 17:08:50 +0900

    Add json file return

--- json_server.rb
+++ json_server.rb
@@ -5,6 +5,16 @@ get '/' do
   params.to_json()
 end
 
+get '/:filename' do
+  fp = "./data/#{params['filename']}.json"
+  if !File.exist?(fp)
+    status 400
+    return "not found #{fp}"
+  end
+  jd = File.read(fp)
+  jd.to_json()
+end
+
 post '/' do
   body = request.body.read
   if body == ''
